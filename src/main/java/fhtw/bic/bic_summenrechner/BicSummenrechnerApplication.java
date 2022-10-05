4bd5147670d548893f71e491cf663d26c301fdbb
DESKTOP-L8J9HD7\Andi <lehnera@technikum-wien.at>
2022-10-05 20:40:17 +0200

    Initial commit

---
+++ src/main/java/fhtw/bic/bic_summenrechner/BicSummenrechnerApplication.java
@@ -0,0 +1,13 @@
+package fhtw.bic.bic_summenrechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicSummenrechnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BicSummenrechnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fhtw/bic/bic_summenrechner/BicSummenrechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.bic.bic_summenrechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicSummenrechnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
