0251902bee92f73a9930575d3f0ff149efca5658
DESKTOP-L8J9HD7\Andi <lehnera@technikum-wien.at>
2022-10-08 21:05:51 +0200

    Added tests to cover acceptance criteria

--- src/test/java/fhtw/bic/bic_summenrechner/SummenrechnerConrollerTest.java
+++ src/test/java/fhtw/bic/bic_summenrechner/SummenrechnerConrollerTest.java
@@ -1,6 +1,8 @@
 package fhtw.bic.bic_summenrechner;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -27,6 +29,22 @@ public class SummenrechnerConrollerTest {
         //Assert
         assertEquals(expectedResult, actualResult);
     }
+
+    @ParameterizedTest
+    @CsvSource({"2,2,4", "-2,8,6","256,1024,1280"})
+    public void SummenrechnerController_shoudReturnCorrectSum_AddIntegers(int numberOne, int numberTwo, long result){
+        //Arrange
+        int firstNumber= numberOne;
+        int secondNUmber= numberTwo;
+        long expectedResult = result;
+
+        //Act
+        int actualResult= testRestTemplate.getForObject("http://localhost:"+port+String.format("/api/summenrechner?numberOne=%s&numberTwo=%s", numberOne,numberTwo), Integer.class);
+
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+
 
 
 }
--- src/test/java/fhtw/bic/bic_summenrechner/SummenrechnerTest.java
+++ src/test/java/fhtw/bic/bic_summenrechner/SummenrechnerTest.java
@@ -1,6 +1,8 @@
 package fhtw.bic.bic_summenrechner;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +17,22 @@ class SummenrechnerTest {
         //Assert
         assertEquals(4,summe);
 
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2,2,4", "-2,8,6","256,1024,1280"})
+    public void SummenrechnerController_shoudReturnCorrectSum_AddIntegers(int numberOne, int numberTwo, long result){
+        //Arrange
+        Summenrechner rechner = new Summenrechner();
+        int firstNumber= numberOne;
+        int secondNUmber= numberTwo;
+        long expectedResult = result;
+
+        //Act
+        long actualResult= rechner.add(numberOne,numberTwo);
+
+        //Assert
+        assertEquals(expectedResult,actualResult);
     }
 
 }
